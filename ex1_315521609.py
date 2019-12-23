1a3d2a583aa337386fafc9e5b2c0b4fcf463f433
RaphaelZwecker <58957897+RaphaelZwecker@users.noreply.github.com>
2019-12-23 09:16:50 +0200

    Add files via upload

---
+++ ex1_315521609.py
@@ -0,0 +1,78 @@
+''' Exercise #1. Computational Thinking and Programming.'''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+# Replace ??? with a string of your choice.
+# Write the rest of the code for question 1 below here.
+name  = 'raphael'
+print('hello ' + name + '!')
+
+
+
+
+#########################################
+# Question 2a - do not delete this comment
+#########################################
+# Replace ??? with a string of your choice.
+# Write the rest of the code for question 2a below here.
+my_str2a = 'what is your favourite color?'
+print(len(my_str2a)) 
+
+
+
+
+#########################################
+# Question 2b - do not delete this comment
+#########################################
+# Replace ??? with a string of your choice.
+# Write the rest of the code for question 2b below here.
+my_str2b = 'what is your favourite color?'
+my_str2b=(str.replace(my_str2b, ' ', ''))
+print(len(my_str2b))
+
+
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+# Replace ??? with a string of your choice.
+# Write the rest of the code for question 3 below here.
+my_str3 ='blue'
+c=my_str3[0:2]
+d=my_str3[2:]
+print((str.lower(c))+'-'+(str.upper(d)))
+
+
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+# Replace ??? with a positive float of your choice.
+# Write the rest of the code for question 4 below here.
+import math
+pi= math.pi
+R  = 8
+a=2*R
+print('size of circle diameter is:',(a))
+b=2*pi*R
+print('circumference is:',(b))
+c=R**2*pi
+print('area is:',(c))
+
+
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+# Replace ??? with a positive int of your choice.
+# Write the rest of the code for question 4 below here.
+repeats = 4
+never='I will never copy homework'
+print((never+"\n")*repeats)
+
+
+
